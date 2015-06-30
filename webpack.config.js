0131233aa8b23cba04359c644252abe23864beab
Buddy Magsipoc <keikun17@gmail.com>
2015-06-30 22:14:37 +0800

    Use babel loader。

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,24 @@ module.exports = {
   entry: "./source/javascripts/entry.js",
   output: {
     filename: "./source/javascripts/bundle.js"
+  },
+
+  module: {
+    loaders: [
+
+      // React and ES5
+      {
+        test: /\.(jsx|js|es6)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel", query: {optional: [], stage: 0} // optional: ['runtime']
+      },
+
+      // Coffeescript
+      { test: /\.coffee$/, loader: "coffee-loader"}
+    ]
+  },
+
+  resolve: {
+    extensions: ['', 'coffee', 'jsx', 'json', 'es6', 'js']
   }
 }
